601ef61ee406a712dafc93b70111203e513bb1e7
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-10-24 17:15:08 +1100

    Migrate to Sinon to resolve mutated arguments

--- 4_programming/lessons/arrays/test.js
+++ 4_programming/lessons/arrays/test.js
@@ -2,7 +2,7 @@
 mocha.setup('bdd');
 mocha.traceIgnores = ['mocha.min.js', 'chai.min.js'];
 var expect = chai.expect;
-var sandbox = chai.spy.sandbox();
+var sandbox = sinon.createSandbox();
 
 describe('functions', function() {
   function r(lower, upper, digits) {
@@ -21,26 +21,14 @@ describe('functions', function() {
     return out;
   }
 
-
-  /*
-    - Create the following array: `roll = ['Jessica', 'Emily', 'Jordan', 'Kayley', 'Bruce', 'Michael', 'Everett', 'Lisa', 'Sam', 'Noah'];`
-    - Log the array (using `console.log()`).
-    - Students take turns each week cleaning the class guinea pig cage. Alert (using `alert()`) the name of the third student on the roll, so they know it's their turn.
-    - Log the length of the array.
-    - Then, add `'James'` to the end of the roll, and log the new roll.
-    - `'Jordan'` changed schools. Remove him, and log the new roll.
-    - `'Michael'` prefers to go by 'Mike'. Change his name, and log the new roll.
-    - (Challenge) Alphabetise the roll and log.
-    - (Challenge) Reverse the array and log.
-    - (Challenge) Log the name of a random student in the class (not tested).
-    - (Challenge) Split the array into two, each with 5 students (one with the first half of the class, the other with the second), and log like `console.log(list1, list2)`.
-    */
   describe('#classRoll()', function() {
     var roll = ['Jessica', 'Emily', 'Jordan', 'Kayley', 'Bruce', 'Michael', 'Everett', 'Lisa', 'Sam', 'Noah'];
 
     before(function() {
-      sandbox.on(window, 'alert', () => 0);
-      sandbox.on(console, 'log', () => 0);
+      sandbox.stub(console, 'log').callsFake(function(arg) {
+          return arg.constructor === Array ? arg.slice(0) : arg;
+      });
+      sandbox.stub(window, 'alert');
       classRoll();
     });
     after(function() {
@@ -48,25 +36,25 @@ describe('functions', function() {
     });
 
     it('should log the initial array', function() {
-      expect(console.log).on.nth(1).be.called.with(roll);
+      expect(console.log.getCall(0)).to.have.returned(roll);
     });
     it('should alert the third child', function() {
-      expect(alert).on.nth(1).be.called.with('Jordan');
+      expect(window.alert.getCall(0)).to.be.calledWith('Jordan');
     });
     it('should log the length of the array', function() {
-      expect(console.log).on.nth(2).be.called.with(10);
+      expect(console.log.getCall(1)).to.be.calledWith(10);
     });
     it('should log the roll with James added', function() {
       roll.push('James');
-      expect(console.log).on.nth(3).be.called.with(roll);
+      expect(console.log.getCall(2)).to.have.returned(roll);
     });
     it('should log the roll with Jordan removed', function() {
-      roll.splice(3,1);
-      expect(console.log).on.nth(4).be.called.with(roll);
+      roll.splice(2,1);
+      expect(console.log.getCall(3)).to.have.returned(roll);
     });
     it("should change Michael's name to Mike", function() {
       roll[4] = 'Mike';
-      expect(console.log).on.nth(5).be.called.with(roll);
+      expect(console.log.getCall(4)).to.have.returned(roll);
     });
   });
 
